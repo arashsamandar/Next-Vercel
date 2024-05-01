206840f25996d6fe19daecfd846b18997d19b4d6
arashsamandar <arash.internet@gmail.com>
2024-05-01 21:28:33 +0330

    adding subfolder and subpages

---
+++ pages/project/about.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+    return(
+        <div>
+            <h1>
+                Project/About SubPage
+            </h1>
+        </div>
+    )
+}
